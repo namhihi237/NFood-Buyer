848151babb66cf49af354e099f6933b8e0f7353f
namhihi237 <yentth237@gmail.com>
2021-11-25 11:45:38 +0700

    profile base

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,6 @@
 import { Text, Box, View, Switch } from "native-base";
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { StyleSheet, TouchableOpacity, ScrollView, Image, TouchableWithoutFeedback } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { useNavigation } from '@react-navigation/native';
 import { useMutation, useQuery } from '@apollo/client';
@@ -8,8 +8,9 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { InputField, ButtonCustom, Toast, Loading } from '../../components';
 import { SCREEN } from "../../constants";
 import { QUERY, client } from '../../graphql';
+import { display, flexDirection } from "styled-system";
 
-const noImage = "https://res.cloudinary.com/do-an-cnpm/image/upload/v1635733754/ck9dkwysgr3inpx6btzn.jpg";
+const noImage = "https://res.cloudinary.com/do-an-cnpm/image/upload/v1637807216/user_ilxv1x.png";
 
 export default function Store(props) {
 
@@ -23,32 +24,69 @@ export default function Store(props) {
   const navigation = useNavigation();
   return (
     <View style={styles.mainContainer}>
-      <View style={styles.infoContainer}>
-        <Image source={{ uri: data ? data.getUser?.image ? data.getUser.image : noImage : noImage }} style={styles.avatar} />
-        <View>
-          <Text bold fontSize="xl">{data?.getUser?.name}</Text>
-          <Text fontSize="lg">{data?.getUser?.phoneNumber}</Text>
+      <View style={styles.header}>
+        <View style={styles.headerIcon}>
+          <FontAwesome5 name="cog" size={hp('2.6%')} color="#fff" style={{ marginRight: 15 }} />
+          <FontAwesome5 name="shopping-cart" size={hp('2.6%')} color="#fff" onPress={() => navigation.navigate(SCREEN.CART)} />
+        </View>
+        <View style={styles.info}>
+          <Image source={{ uri: data ? data?.getUser.avatar || noImage : noImage }} style={styles.avatar} />
+          <View >
+            <Text style={styles.name}>{data?.getUser.name}</Text>
+            <Text style={styles.phone}>{data?.getUser.phoneNumber}</Text>
+          </View>
         </View>
       </View>
-      <View>
-        <TouchableOpacity style={styles.card}>
-          <Text style={styles.title}>Quản lý khuyến mãi</Text>
-          <FontAwesome5 name="arrow-right" size={20} color="#A4A4A4" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.card} onPress={() => navigation.navigate(SCREEN.MENU)}>
-          <Text style={styles.title}>Chỉnh sửa menu</Text>
-          <FontAwesome5 name="arrow-right" size={20} color="#A4A4A4" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.card}>
-          <Text style={styles.title}>Đánh giá của khách hàng</Text>
-          <FontAwesome5 name="arrow-right" size={20} color="#A4A4A4" />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.cardOnline}>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={{ fontSize: 16, fontFamily: 'SF-UI-Text-Regular' }}>Thêm tài khoản tín dụng</Text>
-          <FontAwesome5 name="arrow-right" size={20} color="#A4A4A4" />
-        </TouchableOpacity>
+      <View >
+        <View style={styles.ordersHeader} >
+          <View style={styles.ordersHeaderLeft}>
+            <FontAwesome5 name="list-alt" size={hp('2.6%')} color="#3c63b2" style={{ marginRight: 10 }} />
+            <Text>Đơn hàng</Text>
+          </View>
+          <TouchableWithoutFeedback onPress={() => { navigation.navigate(SCREEN.LIST_ORDERS) }}>
+            <View style={styles.ordersHeaderLeft}>
+              <Text>Tất cả lịch sử mua hàng</Text>
+              <FontAwesome5 name="angle-right" size={hp('2.6%')} color="#000" style={{ marginLeft: 10 }} />
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+        <View style={styles.listOrderContainer}>
+          <TouchableWithoutFeedback>
+            <View style={styles.orderListIcon}>
+              <FontAwesome5 name="wallet" size={hp('2.6%')} color="#000" style={{ marginLeft: 10, marginBottom: 5 }} />
+              <Text>Đang chờ</Text>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback>
+            <View style={styles.orderListIcon}>
+              <FontAwesome5 name="shipping-fast" size={hp('2.6%')} color="#000" style={{ marginLeft: 10, marginBottom: 5 }} />
+              <Text>Đang giao</Text>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback>
+            <View style={styles.orderListIcon}>
+              <FontAwesome5 name="hand-holding-usd" size={hp('2.6%')} color="#000" style={{ marginLeft: 10, marginBottom: 5 }} />
+              <Text>Đã nhận</Text>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback>
+            <View style={styles.orderListIcon}>
+              <FontAwesome5 name="window-close" size={hp('2.6%')} color="#000" style={{ marginLeft: 10, marginBottom: 5 }} />
+              <Text>Đã hủy</Text>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+
+        <TouchableWithoutFeedback>
+          <View style={styles.ordersHeader} >
+            <View style={styles.ordersHeaderLeft}>
+              <FontAwesome5 name="wallet" size={hp('2.6%')} color="#3c63b2" style={{ marginRight: 10 }} />
+              <Text>Ví của tôi</Text>
+            </View>
+            <FontAwesome5 name="angle-right" size={hp('2.6%')} color="#000" style={{ marginLeft: 10 }} />
+          </View>
+        </TouchableWithoutFeedback>
+
       </View>
     </View >
   );
@@ -59,59 +97,67 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
   },
-  infoContainer: {
+  header: {
+    height: hp('16%'),
+    backgroundColor: '#F24F04'
+  },
+  info: {
+    display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    width: wp("100%"),
-    backgroundColor: "#fff",
-    paddingLeft: wp("5%"),
-    paddingBottom: hp("2%"),
-    paddingTop: hp("2%"),
-    marginBottom: 2,
+    marginHorizontal: wp('5%'),
   },
   avatar: {
-    height: 100,
-    width: 100,
-    borderRadius: 6,
-    marginRight: 15
-  },
-  title: {
-    fontSize: 16,
-    color: "#A4A4A4",
-    fontFamily: "SF-UI-Text-Regular",
-  },
-  card: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 15,
-    borderRadius: 6,
-    backgroundColor: 'red',
-    width: wp("100%"),
-    paddingBottom: 20,
-    paddingTop: 20,
-    backgroundColor: '#fff',
-    marginBottom: 2
-  },
-  cardOnline: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    width: wp("100%"),
-    justifyContent: 'center',
-    paddingBottom: 20,
-    paddingTop: 20,
-  },
-  btn: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: '#fff',
-    width: wp("80%"),
-    backgroundColor: '#fff',
-    height: hp("10%"),
-    borderColor: '#A4A4A4',
+    width: hp('8%'),
+    height: hp('8%'),
+    borderRadius: hp('4%'),
+    marginRight: hp('2%'),
     borderWidth: 1,
-    borderRadius: 4,
+    borderColor: '#000'
   },
+  name: {
+    color: '#fff',
+    fontSize: hp('2.6%'),
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  phone: {
+    color: '#fff',
+    fontSize: hp('1.7%')
+  },
+  headerIcon: {
+    width: "100%",
+    display: 'flex',
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    paddingHorizontal: wp('5%'),
+    paddingVertical: hp('1%'),
+  },
+  ordersHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: wp('5%'),
+    backgroundColor: '#fff',
+    marginBottom: 2,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  ordersHeaderLeft: {
+    flexDirection: 'row',
+  },
+  orderListIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    paddingHorizontal: 10
+  },
+  listOrderContainer: {
+    paddingHorizontal: wp('6%'),
+    paddingVertical: hp('1%'),
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 2,
+  }
 
 });

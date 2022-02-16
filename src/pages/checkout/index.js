cd3c0e49770ad828569560d0380b2829c60fcf29
namhihi237 <yentth237@gmail.com>
2022-02-16 22:47:08 +0700

    fix minor

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,6 +1,6 @@
 import { Text, Input, View, Checkbox, Button, Modal } from "native-base";
 import React from "react";
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { WebView } from 'react-native-webview';
 import { SCREEN } from "../../constants";
